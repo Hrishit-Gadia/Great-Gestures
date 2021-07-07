d986ae42b7fdd56fc44fc44cfbb3e5e73f326f57
Hrishit-Gadia <80762914+Hrishit-Gadia@users.noreply.github.com>
2021-07-07 07:46:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+//Variables
+var Emoji;
+var Sentence;
+var Photograph;
+var Model;
+var Gesture;
+var Speaker;
+
+//Setting Webcam
 Webcam.set({
     width: 350,
     height: 308,
@@ -5,3 +14,23 @@ Webcam.set({
     png_format: 100
 });
 Webcam.attach('Webcam');
+
+//Setting Teachable Machines
+Model = imageClassifier("https://teachablemachine.withgoogle.com/models/Zy1FEfObR/model.json", Status);
+
+//Functions
+
+function Status(){
+    console.info("Asd")
+}
+function ClickPic() {
+    Webcam.snap(function (Photograph) {
+        document.getElementById('Pic').innerHTML = "<img id = 'Picture' src = '" + Photograph + "'>"
+    });
+}
+
+function Speak() {
+    Sentence = "You Are Currently Showing" + Gesture + "Gesture";
+    Speaker = window.speechSynthesis;
+    Speaker.speak(Sentence);
+}
